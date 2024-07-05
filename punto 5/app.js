fc80b32ee8a6a37b44bae999b9a900dbeec8f6a3
scorhenao <samcorhenao@gmail.com>
2024-07-05 19:12:42 -0400

    adding filter method

--- punto 5/app.js
+++ punto 5/app.js
@@ -19,13 +19,13 @@ class GestorProducts {
         const productsContainer = document.getElementById('products-container');
         products.forEach(product => {
             const productElement = document.createElement('div');
-            productElement.classList.add("col", "w-50", "bg-gradient", "m-5", "p-2", "rounded");
+            productElement.classList.add("col", "w-50", "bg-gradient", "m-5", "p-2", "rounded", "shadow-lg");
             productElement.innerHTML = `
                 <h3>${product.name}</h3>
                 <p>Category: ${product.category}</p>
                 <p>Price: ${product.price}</p>
                 <p>Stock: ${product.stock}</p>
-                <button class="btn btn-success">Comprar</button>
+                <button class="btn btn-primary">Comprar</button>
             `;
             productsContainer.appendChild(productElement);
         });
@@ -35,14 +35,53 @@ class GestorProducts {
         const containerTotal = document.getElementById("total-price-products");
         const sumProducts = this.products.reduce((acc, product) => acc + product.price, 0);
         containerTotal.innerHTML = `
-            <h3>Total: $${sumProducts}</h3>
+            <button class="btn btn-success no-pointer w-25 text-center">Total: $${sumProducts}</button>
         `;
+    }
+
+    filterByCategory() {
+        const form = document.getElementById("filter-form");
+        const containerProducts = document.getElementById("products-container");
+    
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            let category = document.getElementById("category-filter").value;
+            containerProducts.innerHTML = ""; // Limpiar productos existentes
+    
+            if (category === "Todos") {
+                // Mostrar todos los productos
+                this.renderProducts();
+            } else {
+                // Filtrar productos por categoría
+                const filteredProducts = this.products.filter(product => product.category === category);
+    
+                if (filteredProducts.length > 0) {
+                    // Mostrar productos filtrados
+                    filteredProducts.forEach(product => {
+                        const productElement = document.createElement('div');
+                        productElement.classList.add("col", "w-50", "bg-gradient", "m-5", "p-2", "rounded", "shadow-lg");
+                        productElement.innerHTML = `
+                            <h3>${product.name}</h3>
+                            <p>Category: ${product.category}</p>
+                            <p>Price: ${product.price}</p>
+                            <p>Stock: ${product.stock}</p>
+                            <button class="btn btn-primary">Comprar</button>
+                        `;
+                        containerProducts.appendChild(productElement);
+                    });
+                } else {
+                    // Mostrar mensaje de error si no hay productos en la categoría
+                    containerProducts.innerHTML = "No hay productos en esta categoría";
+                }
+            }
+        });
     }
 
     loadProducts() {
         try {
             this.renderProducts();
             this.totalPriceProducts();
+            this.filterByCategory();
         } catch (error) {
             console.warn(error);
         }
